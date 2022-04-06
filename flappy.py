f9c31429df3c80973dbc2b1b9ae0c843232d42a6
jeffersonvalente <jeffersonvalente1998@gmail.com>
2022-04-05 23:17:08 -0300

    adicionado colisão canos

--- flappy.py
+++ flappy.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 from pygame.locals import *
 
 #define tamanho da tela
@@ -10,6 +10,12 @@ GAMESPEED = 10
 
 GROUND_WIDTH = 2 * SCREEN_WIDTH
 GROUND_HEIGHT = 100
+
+PIPE_WIDTH = 80
+PIPE_HEIGHT = 500
+PIPE_GAP = 200
+
+
 
 #cria o passarinho
 class Bird(pygame.sprite.Sprite):
@@ -25,8 +31,7 @@ class Bird(pygame.sprite.Sprite):
         self.speed = SPEED
         
         
-        self.current_image = 0
-        
+        self.current_image = 0  
         
         
         #chama a imagem e converte os pixels transparentes (convert_alpha)
@@ -51,6 +56,30 @@ class Bird(pygame.sprite.Sprite):
        
     def bump(self):
         self.speed = -SPEED #passaro sobe
+    
+#cria os canos
+class Pipe(pygame.sprite.Sprite):
+    
+    def __init__(self, inverted, xpos, ysize,):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = pygame.image.load('F:/flappy_bird_python/sprites/pipe-red.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (PIPE_WIDTH, PIPE_HEIGHT))
+        self.mask = pygame.mask.from_surface(self.image) #mascara de colisão
+        
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+        
+        if inverted:
+            self.image = pygame.transform.flip(self.image, False, True) # flipa o cano ao contrario
+            self.rect[1] = - (self.rect[3] -ysize)
+        else:
+            self.rect [1] = SCREEN_HEIGHT - ysize
+        
+        self.mask = pygame.mask.from_surface(self.image)
+    
+    def update(self):
+        self.rect[0] -= GAMESPEED
         
 class Ground(pygame.sprite.Sprite): #cria a base/chao
     
@@ -69,6 +98,14 @@ class Ground(pygame.sprite.Sprite): #cria a base/chao
 
 def is_off_screen(sprite):
     return sprite.rect[0] < -(sprite.rect[2])
+
+#cria os canos randomicamente
+def get_random_pipes(xpos):
+    size = random.randint(100 , 300)
+    pipe = Pipe(False, xpos, size)
+    pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
+    return (pipe, pipe_inverted)
+    
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -90,7 +127,12 @@ for i in range(2):
 
 clock = pygame.time.Clock() #define o fps/velocidade do bate asa
 
-
+#adiciona grupo de canos
+pipe_group = pygame.sprite.Group()
+for i in range(2):
+    pipes = get_random_pipes(SCREEN_WIDTH * i + 800)
+    pipe_group.add(pipes[0])
+    pipe_group.add(pipes[1])
 #cria laço principal do jogo
 while True:
     clock.tick(30)
@@ -113,16 +155,28 @@ while True:
         new_ground = Ground(GROUND_WIDTH - 20)
         ground_group.add(new_ground)
         
+    if is_off_screen(pipe_group.sprites()[0]):
+        pipe_group.remove(pipe_group.sprites()[0])
+        pipe_group.remove(pipe_group.sprites()[0])
+        
+        pipes = get_random_pipes(SCREEN_WIDTH * 2)
+        pipe_group.add(pipes[0])
+        pipe_group.add(pipes[1])
+       
+        
     bird_group.update() #muda opassarinho conforme o game
     ground_group.update()
+    pipe_group.update()
     
     #desenha os elementos do grupo bird
     bird_group.draw(screen)
+    pipe_group.draw(screen)
     #desenha os elementos do grupo ground
     ground_group.draw(screen)
     
     #criando colisão
-    if pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask):
+    if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or 
+        pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
         input()
         break
     
